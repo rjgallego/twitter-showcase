1415b5cf3e05b13f2cb8102c39accdab7fa68f8c
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-18 19:23:58 -0700

    Adding route to get user's tweets by username - route /:username uses the username in the URl params, uses the Search URL to serach for the latest 10 tweets by that user, then return the info converted by the getTweetInfo function - getTweetInfo: function to use the Status search URL to get more information on the tweet by ID, including media queries, artist info, etc., then returns only the info needed for the front-end as an object for each tweet send as a paramter

--- backend/routes/discoverRouter.js
+++ backend/routes/discoverRouter.js
@@ -1,5 +1,48 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const discoverRouter = express.Router();
 
+const STATUS_URL = 'https://api.twitter.com/1.1/statuses/show.json?id'
+const SEARCH_URL = 'https://api.twitter.com/2/tweets/search/recent?query=from'
+const SEARCH_PARAMETERS = 'tweet.fields=created_at&expansions=author_id&user.fields=created_at';
+
+const options = {
+    headers: {
+        'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
+    }
+}
+
+discoverRouter.get('/:username', (req, res) => {
+    const username = req.params.username;
+    axios.get(`${SEARCH_URL}:${username}&${SEARCH_PARAMETERS}`, options)
+        .then(result => result.data)
+        .then(resultData => {
+            getTweetInfo(resultData.data).then(tweetData => res.send(tweetData));
+        })
+        .catch(error => {
+            res.status(error.response.status).send({
+                "error": error.response.statusText
+            })
+        })
+})
+
+const getTweetInfo = async (tweetArray) => {
+    return await Promise.all(tweetArray.map( async (tweet) => {
+        const tweetInfo = await axios.get(`${STATUS_URL}=${tweet.id}`, options);
+        const mediaUrl = tweetInfo.data.entities.media ? tweetInfo.data.entities.media[0].media_url_https : "" 
+
+        return {
+            'id': tweet.id,
+            'text': tweetInfo.data.text,
+            'media_url': mediaUrl,
+            'created_at': tweetInfo.data.created_at,
+            'user': {
+                'name': tweetInfo.data.user.name,
+                'screen_name': tweetInfo.data.user.screen_name,
+                'profile_img_url': tweetInfo.data.user.profile_image_url_https
+            }
+        } 
+    }))
+}
 
 module.exports = discoverRouter;
