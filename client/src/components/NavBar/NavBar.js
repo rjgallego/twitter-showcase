0222eea2940e424128d70b0b45616fb0688fa52d
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-20 18:00:54 -0700

    Changing outer div to a nav component, and changing class to className for each div

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -4,11 +4,11 @@ import './NavBar.css'
 class NavBar extends React.Component {
     render() {
         return(
-            <div id="main">
-                <div class="nav-option selected">Home</div>
-                <div class="nav-option">Search</div>
-                <div class="nav-option">Discover</div>
-            </div>
+            <nav id="main">
+                <div className="nav-option selected">Home</div>
+                <div className="nav-option">Search</div>
+                <div className="nav-option">Discover</div>
+            </nav>
         )
     }
 }
