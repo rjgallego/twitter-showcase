89a4f096d4352819b18a51af938512856332d1dc
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-20 18:02:42 -0700

    Adding SearchPage.js file for later implementation

---
+++ client/src/components/SearchPage/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageLayout from '../PageLayout';
+
+class SearchPage extends React.Component {
+    render() {
+        return (
+            <PageLayout></PageLayout>
+        )
+    }
+}
+
+export default SearchPage;
