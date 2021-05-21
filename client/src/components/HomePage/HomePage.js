cc6cdca7fb95d190f144442aa8b88e2960fc115e
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-20 18:01:38 -0700

    Adding HomePage that sends div elements to PageLayout as props to render, plus .css file for styling

---
+++ client/src/components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div className="text-div">
+            <div id="title">
+                <div id="sub-title">Using Twitter to Discover</div>
+                <div id="main-title">Electronic Artists</div>
+            </div>
+        </div>
+    )
+    
+}
+
+export default HomePage;
