f398c5cf54096bf1b55cb66c7770cfe06374158e
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-20 18:02:27 -0700

    Adding DiscoverPage.js file for later implementation

---
+++ client/src/components/DiscoverPage/DiscoverPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageLayout from '../PageLayout';
+
+class DiscoverPage extends React.Component {
+    render() {
+        return (
+            <PageLayout></PageLayout>
+        )
+    }
+}
+
+export default DiscoverPage;
