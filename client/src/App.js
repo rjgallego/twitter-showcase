d16be5eb4f23a3043f5d0eddc0f33dc6115f899b
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-20 18:03:33 -0700

    Adding pages and router to route through different page components within PageLayout fragment component using react-router-dom BrowserRouter and Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import DiscoverPage from './components/DiscoverPage/DiscoverPage';
+import SearchPage from './components/SearchPage/SearchPage';
+import PageLayout from './components/PageLayout';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <BrowserRouter>
+        <PageLayout>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/search" component={SearchPage} />
+          <Route path="/discover" component={DiscoverPage} />
+        </PageLayout>
+      </BrowserRouter>
     </div>
   );
 }
