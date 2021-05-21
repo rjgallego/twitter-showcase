6636d5255e38fac6b2c2754288c4453f274babc0
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-20 17:01:09 -0700

    Adding page layout; contains logo, background image, and div for displaying main content

---
+++ client/src/components/PageLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './NavBar.js';
+import './PageLayout.css'
+
+class PageLayout extends React.Component {
+    render() {
+        return (
+            <div id="body">
+                <Navbar />
+                <div id="main-content"></div>
+                <div id="logo">DROPS</div>
+            </div>
+        );
+    }
+}
+
+export default PageLayout;
