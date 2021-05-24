3557c1c2d95c2b13d940a5f58219876689d89fa4
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-23 17:35:56 -0700

    Adding new components folder and moving NavBar as subfolder from pages folder

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {changeElementClass} from '../../helpers';
 import './NavBar.css'
 
-class NavBar extends React.Component {
-    render() {
-        return(
-            <nav id="main">
-                <div className="nav-option selected">Home</div>
-                <div className="nav-option">Search</div>
-                <div className="nav-option">Discover</div>
-            </nav>
-        )
+const NavBar = () => {
+
+    const handleClick = (event) => {
+        changeElementClass(event.target.id, '.nav-option', 'selected')
     }
+
+    return(
+        <nav id="main">
+            <Link to='/' id="home-link" className="nav-option selected" onClick={handleClick}>Home</Link>
+            <Link to='/search' id="search-link" className="nav-option" onClick={handleClick}>Search</Link>
+            <Link to='/discover' id="discover-link" className="nav-option" onClick={handleClick}>Discover</Link>
+        </nav>
+    )
+    
 }
 
 export default NavBar;
