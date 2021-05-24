bd1d83f4b730cab75bd87d7b299568c1c4140cf2
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-23 17:30:52 -0700

    Adding options buttons for selecting artists and function to handle onClick

---
+++ client/src/pages/DiscoverPage/DiscoverPage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './DiscoverPage.css';
+import {changeElementClass} from '../../helpers';
+
+const DiscoverPage = () => {
+
+    const handleClick = (event) => {
+        changeElementClass(event.target.id, '.option-button', 'selected-option');
+    }
+
+    const setSelectedOptionClass = (id) => {
+        const navOptions = document.querySelectorAll('.option-button');
+        navOptions.forEach((e, i) => {
+            if(e.id === id){
+                e.classList.add('selected-option');
+                return;
+            }
+            e.classList.remove('selected-option');
+        })
+    }
+
+    return (
+        <div id="discover-content">
+            <div id="artist-options">
+                <div id="artist-1" className="option-button selected-option" onClick={handleClick}>Illenium</div>
+                <div id="artist-2" className="option-button" onClick={handleClick}>Alan Walker</div>
+                <div id="artist-3" className="option-button" onClick={handleClick}>Kygo</div>
+                <div id="artist-4" className="option-button" onClick={handleClick}>Galantis</div>
+                <div id="artist-5" className="option-button" onClick={handleClick}>KSMR</div>
+            </div>
+        </div>
+    )
+
+}
+
+export default DiscoverPage;
