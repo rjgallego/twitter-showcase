2a743805bf229f86d3aa94c3bd23a28239f34f04
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-25 15:48:35 -0700

    Updating Twitter Card - adding 'alt' property to img elements to remove warning - adding conditional for media_url in case there is none returned for the tweet, will not display img element and adjust size of tweet card based on whether or not img is there

--- client/src/components/TwitterCard/TwitterCard.js
+++ client/src/components/TwitterCard/TwitterCard.js
@@ -4,17 +4,21 @@ import './TwitterCard.css';
 const TwitterCard = ({ TweetData }) => {
 
     const convertTimestamp = () => {
-        const timestamp = new Date(TweetData.created_at)
-        const timestampArray = timestamp.toString().split(' ');
+        const tweetCreatedAt = new Date(TweetData.created_at);
+        const timestampArray = tweetCreatedAt.toString().split(' ');
         const timeArray = timestampArray[4].split(':');
         const time = parseInt(timeArray[0]) < 12 ? `${timeArray[0]}:${timeArray[1]} AM` : `${timeArray[0]}:${timeArray[1]} PM`
         return `${time} - ${timestampArray[1]} ${timestampArray[2]}, ${timestampArray[3]}`
     }
 
+    // const displayMediaImg = () => {
+    //     return 
+    // }
+
     return (
         <div id="card-main">
             <div id="artist-info">
-                <img id="artist-profile-img" src={TweetData.user.profile_img_url}/>
+                <img id="artist-profile-img" src={TweetData.user.profile_img_url} alt="Twitter user's profile img"/>
                 <div>
                     <div id="artist-name">{TweetData.user.name}</div>
                     <div id="artist-screen-name">@{TweetData.user.screen_name}</div>
@@ -22,7 +26,12 @@ const TwitterCard = ({ TweetData }) => {
             </div>
             <div id="tweet-text">{TweetData.text}</div>
             <div id="tweet-media-div">
-                <img id="tweet-media-content" src={TweetData.media_url} />
+                {
+                    TweetData.media_url.length === 0 ? "" 
+                        : <img id="tweet-media-content" 
+                                src={TweetData.media_url} 
+                                alt="Tweet media image"/>
+                }
             </div>
             <div id="tweet-timestamp">{convertTimestamp()}</div>
         </div>
