af96ee1ddef320cd16e19e8a51674efddebc7837
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-25 15:38:33 -0700

    Connecting Discover page to backend and storing tweets in state as array - state: holds array of tweet data retrieved from backend - useEffect: get list of tweets from backend on-load - handleClick: call getTweetArray every time an artist button is clicked - HTML : check if tweetArray has no data, if so then display Loader else display TwitterCard with tweet data, also change ID of each artist button to pass on to getTweetArray method on click

--- client/src/pages/DiscoverPage/DiscoverPage.js
+++ client/src/pages/DiscoverPage/DiscoverPage.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loader from 'react-loader-spinner';
+
+import TwitterCard from '../../components/TwitterCard/TwitterCard';
 import './DiscoverPage.css';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import {changeElementClass} from '../../helpers';
 
 const DiscoverPage = () => {
+    const [tweetArray, setTweetArray] = useState([]);
+    const [tweetIndex, setTweetIndex] = useState(0);
+    const DISCOVER_URL = "http://localhost:8080/user?username"
+
+    useEffect(() => {
+        getTweetArray('illenium')
+        console.log(tweetArray);
+    }, [])
+
+    const getTweetArray = (username) => {
+        axios.get(`${DISCOVER_URL}=${username}`)
+            .then(result => result.data)
+            .then(resultData => setTweetArray(resultData));
+    }
 
     const handleClick = (event) => {
         changeElementClass(event.target.id, '.option-button', 'selected-option');
+        getTweetArray(event.target.id);
     }
 
     return (
         <div id="discover-content">
+            <div id="page-content">
+                {
+                    tweetArray.length === 0 ?
+                        <Loader type="Bars" color="#FFFFFF" height={80} width={80} />
+                    :   <TwitterCard TweetData={tweetArray[tweetIndex]} />
+                }
+            </div>
             <div id="artist-options">
-                <div id="artist-1" className="option-button selected-option" onClick={handleClick}>Illenium</div>
-                <div id="artist-2" className="option-button" onClick={handleClick}>Alan Walker</div>
-                <div id="artist-3" className="option-button" onClick={handleClick}>Kygo</div>
-                <div id="artist-4" className="option-button" onClick={handleClick}>Galantis</div>
-                <div id="artist-5" className="option-button" onClick={handleClick}>KSMR</div>
+                <div id="illenium" className="option-button selected-option" onClick={handleClick}>Illenium</div>
+                <div id="iamalanwalker" className="option-button" onClick={handleClick}>Alan Walker</div>
+                <div id="kygomusic" className="option-button" onClick={handleClick}>Kygo</div>
+                <div id="wearegalantis" className="option-button" onClick={handleClick}>Galantis</div>
+                <div id="KSHMRmusic" className="option-button" onClick={handleClick}>KSHMR</div>
             </div>
         </div>
     )
