1aab32417a00a18dec0ed9e21b7177b6518bd621
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-25 15:52:37 -0700

    installing cors package and adding it to server.js file to allow requests from front end (localhost:3000)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 const searchRouter = require('./routes/searchRouter');
 const userRouter = require('./routes/userRouter');
