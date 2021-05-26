472fc07710336f08ea3eed5297baea790be45895
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-26 15:31:05 -0700

    moving scroll listener to the Page Layout to include all pages instead of just home page

--- client/src/pages/PageLayout.js
+++ client/src/pages/PageLayout.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
 import Navbar from '../components/NavBar/NavBar.js';
 import './PageLayout.css'
 
 const PageLayout = ({children}) => {
+    useEffect(() => {
+        setScrollListener();
+    }, [])
+
+    const setScrollListener = () => {
+        const container = document.getElementById('main-content');
+        container.addEventListener('scroll', () => {
+            const textContent = document.querySelectorAll('.scroll-div')
+            textContent.forEach((e, i) => {
+                const top = (e.getBoundingClientRect().top);
+                if(top < 200 && top > -150) {
+                    e.classList.add('in-view')
+                    setInvisibleClass(e)
+                } else {
+                    e.classList.remove('in-view');
+                }
+            })
+        })
+    }
+
+    const setInvisibleClass = (element) => {
+        const pointer = document.querySelector('.pointer');
+        if(element.classList.contains('end')){
+            pointer.style.display = 'none';
+            return;
+        }
+        pointer.style.display = 'block';
+    }
+
     return (
         <React.Fragment>
             <div id="body">
