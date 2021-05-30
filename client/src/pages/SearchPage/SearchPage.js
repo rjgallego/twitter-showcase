38c20de9478898968ca19781373b7c30dd40b17e
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-30 15:15:40 -0700

    display loading spinner when calling API, and reset tweetArray to 0 when input text box is blank

--- client/src/pages/SearchPage/SearchPage.js
+++ client/src/pages/SearchPage/SearchPage.js
@@ -6,6 +6,7 @@ import {getTweetsByUsername, getTweetsByKeyword, createTweetDivs} from '../../he
 const SearchPage = () => {
     const [tweetArray, setTweetArray] = useState([]);
     const [searchInput, setSearchInput] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleInput = (event) => {
         setSearchInput(event.target.value);
@@ -13,14 +14,22 @@ const SearchPage = () => {
 
     const handleEnter = async (event) => {
         if(event.code !== "Enter") return;
+        if(searchInput.length === 0){
+            setTweetArray([])
+            return;
+        } 
         let returnedArray;
 
         if(searchInput.indexOf('@') > -1) {
+            setIsLoading(true);
             returnedArray = await getTweetsByUsername(searchInput.slice(1))
+            setIsLoading(false);
             setTweetArray(returnedArray)
         }
         else {
+            setIsLoading(true);
             returnedArray = await getTweetsByKeyword(searchInput)
+            setIsLoading(false);
             setTweetArray(returnedArray)
         }
 
@@ -36,14 +45,14 @@ const SearchPage = () => {
                     onKeyUp={handleEnter} />
             </div>
             {
-                tweetArray.length === 0 ?
+                isLoading ?
                     <div id="loader-div" className="page-content">
                         <Loader type="Bars" color="#FFFFFF" height={80} width={80} />
                     </div>
                 :   createTweetDivs(tweetArray)
             }
             {
-                tweetArray.length === 1 ? <div className="pointer"></div> : <div id="scroll-pointer" className="pointer">V</div>
+                tweetArray.length <= 1 ? <div className="pointer"></div> : <div id="scroll-pointer" className="pointer">V</div>
             }
         </div>
     )
