ba7441a807cc710bc0c0dac63224be0a31c6cdab
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-30 13:50:10 -0700

    Creating Search page - returning main div with text input, array of tweet divs, and pointer

--- client/src/pages/SearchPage/SearchPage.js
+++ client/src/pages/SearchPage/SearchPage.js
@@ -1,12 +1,86 @@
-import React from 'react';
-import PageLayout from '../PageLayout';
+import React, {useState} from 'react';
+import Loader from 'react-loader-spinner';
+import axios from 'axios';
 
-class SearchPage extends React.Component {
-    render() {
-        return (
-            <PageLayout></PageLayout>
-        )
+import './SearchPage.css';
+
+import TwitterCard from '../../components/TwitterCard/TwitterCard';
+
+const KEYWORD_URL = 'http://localhost:8080/search/content?q';
+const USER_URL = 'http://localhost:8080/user?username';
+
+const SearchPage = () => {
+    const [tweetArray, setTweetArray] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+
+    const handleInput = (event) => {
+        setSearchInput(event.target.value);
     }
+
+    const handleEnter = (event) => {
+        if(event.code !== "Enter") return;
+
+        if(searchInput.indexOf('@') > -1) {
+            getTweetsByUsername(searchInput.slice(1))
+        }
+        else getTweetsByKeyword(searchInput)
+
+    }
+
+    const getTweetsByUsername = (username) => {
+        axios.get(`${USER_URL}=${username}`)
+            .then(result => result.data)
+            .then(resultData => setTweetArray(resultData));
+    }
+
+    const getTweetsByKeyword = (keyword) => {
+        axios.get(`${KEYWORD_URL}=${keyword}`)
+            .then(result => result.data)
+            .then(resultData => setTweetArray(resultData));
+    }
+
+    const createTweetDivs = () => {
+        return tweetArray.map((tweetData, i) => {
+            if(i === 0) {
+                return  <div key={i} className="page-content in-view scroll-div">
+                            <TwitterCard TweetData={tweetData} />
+                        </div>
+            }
+            if(i === (tweetArray.length - 1)) {
+                return  <div key={i} className="page-content scroll-div end">
+                            <TwitterCard TweetData={tweetData} />
+                        </div>
+            }
+
+            return  <div key={i} className="page-content scroll-div">
+                        <TwitterCard TweetData={tweetData} />
+                    </div>
+        })
+    }
+
+
+    return (
+        <div id="search-main">
+            <div id="input-div">
+                <input id="search-input" 
+                    type="text" 
+                    placeholder="Search by @user or keyword, then Enter..." 
+                    onInput={handleInput}
+                    onKeyUp={handleEnter} />
+            </div>
+            {
+                tweetArray.length === 0 ?
+                    <div id="loader-div" className="page-content">
+                        <Loader type="Bars" color="#FFFFFF" height={80} width={80} />
+                    </div>
+                :   createTweetDivs()
+            }
+            {
+                tweetArray.length === 1 ? <div className="pointer"></div> : <div id="scroll-pointer" className="pointer">V</div>
+            }
+        </div>
+    )
+    
 }
 
 export default SearchPage;
