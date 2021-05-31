89922c9e671fe65c9942432cd35321b81bfbecef
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-05-31 11:03:46 -0700

    Adding express serve static files in case of Production environment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
+
+if(process.env.ENVIRONMENT === 'prod'){
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    });
+}
 
 const searchRouter = require('./routes/searchRouter');
 const userRouter = require('./routes/userRouter');
@@ -14,5 +23,7 @@ const port = 8080;
 
 app.use('/search', searchRouter);
 app.use('/user', userRouter);
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
