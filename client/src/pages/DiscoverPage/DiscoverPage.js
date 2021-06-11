ccc199a3ef6c0539b8f345554aea4b7733ff914f
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-10 18:52:15 -0700

    adding onClick to each function and removing addClickHandlers function, removing changeelementClass and updating element clases using state/ ternary instead

--- client/src/pages/DiscoverPage/DiscoverPage.js
+++ client/src/pages/DiscoverPage/DiscoverPage.js
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Loader from 'react-loader-spinner';
 import './DiscoverPage.css';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import {changeElementClass, createTweetDivs, getTweetData} from '../../helpers';
+import {createTweetDivs, getTweetData} from '../../helpers';
 
 const USER_URL = '/user?username';
-
 
 const DiscoverPage = () => {
     const [tweetArray, setTweetArray] = useState([]);
@@ -14,7 +13,6 @@ const DiscoverPage = () => {
 
     useEffect(() => {
         updateTweetArray(USER_URL, selectedOption)
-        addClickHandlers();
     }, [selectedOption])
 
     const updateTweetArray = (URL, searchTerm) => {
@@ -27,13 +25,7 @@ const DiscoverPage = () => {
     }
 
     const handleClick = (event) => {
-        changeElementClass(event.target.id, '.option-button', 'selected-option');
         setSelectedOption(event.target.id)
-    }
-
-    const addClickHandlers = () => {
-        document.querySelectorAll('.option-button')
-            .forEach(option => option.addEventListener('click', handleClick))
     }
 
     return (
@@ -51,11 +43,21 @@ const DiscoverPage = () => {
                   : <div id="scroll-pointer" className="pointer">V</div>
             }
             <div id="artist-options">
-                <div id="illenium" className="option-button selected-option">Illenium</div>
-                <div id="iamalanwalker" className="option-button">Alan Walker</div>
-                <div id="kygomusic" className="option-button">Kygo</div>
-                <div id="wearegalantis" className="option-button">Galantis</div>
-                <div id="KSHMRmusic" className="option-button">KSHMR</div>
+                <div id="illenium" 
+                     className={`option-button ${selectedOption === 'illenium' ? 'selected-option' : ''}`}
+                     onClick={handleClick}>Illenium</div>
+                <div id="iamalanwalker" 
+                     className={`option-button ${selectedOption === 'iamalanwalker' ? 'selected-option' : ''}`}
+                     onClick={handleClick}>Alan Walker</div>
+                <div id="kygomusic" 
+                     className={`option-button ${selectedOption === 'kygomusic' ? 'selected-option' : ''}`}
+                     onClick={handleClick}>Kygo</div>
+                <div id="wearegalantis" 
+                     className={`option-button ${selectedOption === 'wearegalantis' ? 'selected-option' : ''}`}
+                     onClick={handleClick}>Galantis</div>
+                <div id="KSHMRmusic" 
+                     className={`option-button ${selectedOption === 'KSHMRmusic' ? 'selected-option' : ''}`}
+                     onClick={handleClick}>KSHMR</div>
             </div>
         </div>
     )
