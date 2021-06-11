d2e67f8ed66e02a2410d76762fc56fbd3b6cc995
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-10 18:53:08 -0700

    consolidating all scroll logic into handleScroll, removing setInvisibleClass function, and removing unneeded useEffect to set scroll listener

--- client/src/pages/PageLayout.js
+++ client/src/pages/PageLayout.js
@@ -1,42 +1,26 @@
-import React, { useEffect }  from 'react';
+import React from 'react';
 import Navbar from '../components/NavBar/NavBar.js';
-import './PageLayout.css'
+import './PageLayout.css';
 
 const PageLayout = ({children}) => {
-    useEffect(() => {
-        setScrollListener();
-    }, [])
 
-    const setScrollListener = () => {
-        const container = document.getElementById('main-content');
-        container.addEventListener('scroll', () => {
-            const textContent = document.querySelectorAll('.scroll-div')
+    const handleScroll = () => {
+        const textContent = document.querySelectorAll('.scroll-div')
             textContent.forEach((e, i) => {
                 const top = (e.getBoundingClientRect().top);
                 if(top < 200 && top > -150) {
                     e.classList.add('in-view')
-                    setInvisibleClass(e)
-                } else {
-                    e.classList.remove('in-view');
-                }
+                    if (e.classList.contains('end')) document.querySelector('.pointer').style.display = 'none';
+                    else document.querySelector('.pointer').style.display = 'block';
+                } else e.classList.remove('in-view');
             })
-        })
-    }
-
-    const setInvisibleClass = (element) => {
-        const pointer = document.querySelector('.pointer');
-        if(element.classList.contains('end')){
-            pointer.style.display = 'none';
-            return;
-        }
-        pointer.style.display = 'block';
     }
 
     return (
         <React.Fragment>
             <div id="body">
                 <Navbar />
-                <div id="main-content">{children}</div>
+                <div id="main-content" onScroll={handleScroll}>{children}</div>
                 <div id="logo">DROPS</div>
             </div>
         </ React.Fragment>
